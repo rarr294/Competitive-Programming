a59605b2150918951fb114197dce812dc4bdd48f
rarr <111408882+rarr294@users.noreply.github.com>
2026-03-13 11:44:43 +0000

    Create octoLetter.c

---
+++ octoLetter.c
@@ -0,0 +1,62 @@
+/*
+
+Everybody knows what an octopus is. You've probably seen videos where they use their tentacles to manipulate objects. 
+Opening jars, picking sticks... You name it, they can toy with it.
+
+But have you ever seen an octopus doing paperwork? Well, 
+you probably never will, but at least you can simulate the idea.
+
+Your task is to simulate an octopus typing in a keyboard. 
+The octopus receives the files by email and reads them in the office,
+but your job is only to process the text content inside those files via a given String.
+
+It can receive an email with an empty file (filled only with spaces), 
+which means that it doesn't type.
+
+An octopus has eight tentacles,so it can type up to eight letters at once.
+Each round consists of the octopus typing as many letters as it can, up to eight.
+Sounds efficient, right? Well, there's a reason why companies don't hire octopuses.
+Due to their suction cups, the tentacles get stuck on the keys they press, 
+not letting them type that letter, space or symbol again until the next round.
+Upper case letters will count as their lower case counterpart, not being able to write "Aa". 
+However, thanks to the number pad numbers will be able to be typed two times each round.
+
+The missing letters will be represented with '*'.
+
+Examples:
+"Letter" -> "Let**r"
+"Unconcerned" -> "Unco**erned"
+"Can a can contain fish?" -> "Can ****n co*tain fish?"
+"Write 122 pages" -> "Write 122 pages"
+"8000 dollars went missing" -> "800* dollars went mis**ng"
+"             " -> "" (counts as empty file)
+
+*/
+
+#include <string.h>
+
+typedef unsigned int uint32t;
+
+void letter(char* s){
+  int size = strlen(s);
+
+  uint32t freq[256];
+
+  for(int a = 0;a < size;a++){
+    if((a % 8) == 0){
+      memset(freq,0,4 * 256);
+    }
+    char msg = *(s + a);
+
+    freq[
+    (msg >= 97 && msg <= 122) ?
+         (msg -= 32) : msg
+    ]++;
+    
+    if((msg >= 65 && msg <= 90) && freq[msg] > 1){
+      *(s + a) = '*';
+    }else if((msg >= 48 && msg <= 57) && freq[msg] > 2){
+      *(s + a) = '*';
+    }
+  }
+}
